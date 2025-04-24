f568367443eec8d8290ffa143bce6e779a9ee9f5
Nicpbear <pepee5815@gmail.com>
2025-04-24 09:55:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,22 +14,22 @@ st.markdown("""
 
 with st.sidebar:
     st.subheader("Propiedades del Tablero")
-    drawing_mode = st.sidebar.selectbox(
+    drawing_mode = st.selectbox(
         "Herramienta de Dibujo:",
         ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
     )
 
-    stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+    stroke_width = st.slider('Ancho de línea', 1, 30, 15)
     stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
-    bg_color = '#100000' 
+    bg_color = st.color_picker("Color de fondo", "#000000") 
 
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_color=bg_color,
-    height=300,
-    width=500,
+    background_color=bg_color,  
+    height=400,  
+    width=600,
     drawing_mode=drawing_mode,
     key="canvas"
 )
